3979253fb94bf2f0870db869150e4f7397f37176
WesleyCh3n <yoyochen0530@gmail.com>
2021-02-03 12:37:25 +0800

    change model path

--- cc/recognition/main.cc
+++ cc/recognition/main.cc
@@ -226,7 +226,7 @@ class CowMonitor{
 int main(){
     if (system("CLS")) system("clear");
     CowMonitor cow_monitor;
-    if(!cow_monitor.Init("yolov4-tiny-416-fp16.tflite"))
+    if(!cow_monitor.Init("../../model/yolov4-tiny-416-fp16.tflite"))
         cerr << "Stop!\n";
     if(!cow_monitor.Stream()) cerr << "Stop!\n";
     return 0;
