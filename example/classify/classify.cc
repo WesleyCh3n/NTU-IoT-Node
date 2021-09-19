89438b08328b1bd376c3811e3f66aec65c6be134
wesleych3n <yoyochen0530@gmail.com>
2021-09-20 00:18:39 +0800

    add arg option to example classify

--- example/classify/classify.cc
+++ example/classify/classify.cc
@@ -7,9 +7,19 @@
 
 
 int main(){
-    MobileNetv2 net("../model/mobilenetv2.tflite");
-    cv::Mat img = cv::imread("./classify.jpg");
-    for(int i=0; i<10; i++){
+    if (argc != 4) {
+        std::cerr << "Usage:\n\t./classify <model path> <repeat time> <img>\n";
+        return 1;
+    }
+
+    std::string model_path(argv[1]);
+    std::string repeat(argv[2]);
+    std::string img_path(argv[3]);
+
+    MobileNetv2 net(model_path);
+    cv::Mat img = cv::imread(img_path);
+
+    for(int i=0; i<std::stoi(repeat); i++){
         std::vector<float> result;
         Timer t;
         result = net.invoke(img);
